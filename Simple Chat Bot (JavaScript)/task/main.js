36a5ba4f2ee4bccf0536ee8e0156798187718e87
ChloeCM <chloecmills@gmail.com>
2024-08-02 13:54:18 +0100

    Beginning project. Simple console chat bot.

---
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -0,0 +1,8 @@
+const input = require("sync-input");
+
+//console.log("Hello, World!");
+
+let botName = String("Delta");
+let dateMade = Number(2024);
+
+console.log(`Hello! My name is ${botName}.\nI was created in ${dateMade}`);
