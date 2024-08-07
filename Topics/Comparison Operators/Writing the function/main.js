f110208ae629f96850cc95643cf696e2623b2531
ChloeCM <chloecmills@gmail.com>
2024-08-08 00:00:22 +0100

    added functionality to count from 0 to the users selected number

---
+++ Topics/Comparison Operators/Writing the function/main.js
@@ -0,0 +1,5 @@
+// Write your code here
+let a = 5;
+let b = "5";
+
+console.log(a === b);
---
+++ Topics/Increment and Decrement/Increment/main.js
@@ -0,0 +1,6 @@
+// Write your code here
+function sum(num1, num2, num3){
+    num1 = num1++;
+
+    return ++num1 + ++num2 + ++num3;
+}
