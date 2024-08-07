8aac7c85604cfebb29010b825372c33d9d6cab2d
ChloeCM <chloecmills@gmail.com>
2024-08-07 16:55:44 +0100

    Read three inputs from the user and calculate the age

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,8 +1,19 @@
-const input = require("sync-input");
+const input = require('sync-input');
 
-//console.log("Hello, World!");
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
 
-let botName = String("Delta");
-let dateMade = Number(2024);
+let name = input();
 
-console.log(`Hello! My name is ${botName}.\nI was created in ${dateMade}`);
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+let remainder3 = Number(input());
+let remainder5 = Number(input());
+let remainder7 = Number(input());
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+console.log(`Your age is ${age}; that's a good time to start programming!`);
